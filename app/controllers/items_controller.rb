34cb07763d07554e322c17af96c99c7ff54ce571
do-taka126 <yousaytakada0112@gmail.com>
2022-07-14 17:26:14 +0900

    レビュー後修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show, :destroy]
-
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :destroy]
 
   def index
     @items = Item.all.order(created_at: "DESC")
@@ -20,18 +20,21 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def destroy
-    item = Item.find(params[:id])
-    if item.destroy
+    if user_signed_in? && current_user.id == @item.user_id
+      @item.destroy
       redirect_to root_path
     end
   end
 
 
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(:name, :price, :detail, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :image).merge(user_id: current_user.id)
