d303c66d424f039e855507eff412fa48620fdb98
do-taka126 <yousaytakada0112@gmail.com>
2022-07-21 10:43:29 +0900

    挙動確認後修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,6 +1,6 @@
 const pay = () => {
   Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY); 
-  const form = document.getElementById("charge-form");
+  const form = document.getElementById("button");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     
